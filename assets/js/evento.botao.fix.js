13d46342c44094f05b5e95a7f53b41d18335249a
Vagner Turella <100965375+vagnerturella@users.noreply.github.com>
2022-04-11 16:04:30 -0300

    Delete assets directory

--- assets/js/evento.botao.fix.js
+++
@@ -1,7 +0,0 @@
-$('#collapse-1').on('show.bs.collapse', function() {
-     $('.botao-nav').css('transform', 'translate(-50%, -40%');
-  });
-
-  $('#collapse-1').on('hide.bs.collapse', function() {
-    $('.botao-nav').css('transform', 'translate(-50%, -95%');
- });
